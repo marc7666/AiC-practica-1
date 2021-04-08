9b9cf2308f5e6455f4ea0978be3f6dc30dae4741
marc7666 <64266158+marc7666@users.noreply.github.com>
2021-04-08 18:03:30 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# ************************************
+# Code made by:
+# Aaron Arenas Tomás
+# Marc Cervera Rosell
+# ************************************
+
+import read_file
+
+if _name_ == "_main_":
+
+ prototypes = read_file("/testing/test5-1.in", data_sep=",", ignore_first_line=True)
+
+ print("\n Prototipes")
+ print("------------------ ")
+ print(prototypes)
