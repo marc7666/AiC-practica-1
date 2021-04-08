9f34526778043000f670dca122091e3563923eb2
marc7666 <64266158+marc7666@users.noreply.github.com>
2021-04-08 17:59:18 +0200

    read_file implemented

---
+++ read_file.py
@@ -0,0 +1,34 @@
+# ************************************
+# Code made by:
+# Aaron Arenas Tomás
+# Marc Cervera Rosell
+# ************************************
+
+def read_file(filename, data_separation=" ", ignore_first_line=False):
+    print("************* READING DATA... *************")
+    values = []
+    # Openning file
+    with open(filename, "r") as fn:
+        # Strip lines
+        strip_reader = (line.strip() for line in fn)
+        # Filter empty lines
+        filtered_reader = (line for line in strip_reader if line)
+        # Skip first line if needed
+        if ignore_first_line:
+            next(filtered_reader)
+        # Split line, parse token and append to values
+        for line in filtered_reader:
+            values.append(
+                [filter_token(token) for token in line.split(data_separation)]
+            )
+        return values
+
+
+def filter_token(token):
+    try:
+        return int(token)
+    except ValueError:
+        try:
+            return float(token)
+        except ValueError:
+            return token
