4ecc1685082bf7bc070782b86883afdc0de5bf1b
aaron-at97 <arenas.aaron97@gmail.com>
2021-04-17 14:12:22 +0200

    comprobacio aqueducte funcional

--- calculs.py
+++ calculs.py
@@ -6,23 +6,30 @@
 
 import math
 
+
 # Calculating posibility create pont
 # disx todas las posiciones del vector
 # posantX siempre el eje x_1
-def calc_impossiblepont(disX, d, h, posantX):
+def calc_impossiblepont(alt, disX, d, h, posantX):
+
     r = (d / 2)
-    height = math.sqrt((r ** 2 - ((disX - posantX - r) ** 2))) + h
-    return height < h
+    height = math.sqrt((r ** 2 - ((disX - posantX - r)**2))) + (h-r)
+    print("height ", height)
+    print("Y ", alt)
+    return height > alt
+
 
 # Calculating posibility create aqueduct
 def calc_impossible(disX, d, h, posantX):
-    r = (d / 2)
 
-    height = math.sqrt((r ** 2 - ((disX - posantX - r) ** 2))) + (h-r)
+    r = (d / 2)
+    height = math.sqrt((r ** 2 - ((disX - posantX - r)**2))) + (h-r)
     return height < h
+
 
 # Calculating the arc radius
 def obtainValues(values):
+
     d = []  # Distance
     disX = []  # Distance Cordenate Sol
     alt = []  # Height
@@ -42,13 +49,16 @@ def obtainValues(values):
 
 
 def medides(values):
+
     x = values[0]
     y = values[1]
 
     return x, y
 
+
 # This method calculates the total costs.
 def costsAque(n, alpha, beta, h, values):
+
     costosaltura = 0
     costosdistancia = 0
     impossible = True
@@ -57,8 +67,8 @@ def costsAque(n, alpha, beta, h, values):
     for i in range(0, n):
         costosaltura += (h - alt[i])
         if 0 < i:
-            impossible = calc_impossible(disX[i], d[i-1], h, disX[i - 1])
-        if i < n-1:
+            impossible = calc_impossible(disX[i], d[i - 1], h, disX[i - 1])
+        if i < n - 1:
             costosdistancia += (d[i] ** 2)
         if not impossible:
             break
@@ -66,6 +76,7 @@ def costsAque(n, alpha, beta, h, values):
     cost = (alpha * costosaltura) + (beta * costosdistancia)
     print(impossible)
     return cost
+
 
 # This method calculates the total costs.
 def costPont(n, alpha, beta, h, values):
@@ -79,7 +90,8 @@ def costPont(n, alpha, beta, h, values):
     for i in range(0, n):
 
         if 0 < i:
-            impossible = calc_impossiblepont(disX[i], dPont, h, disX[0])
+            impossible = calc_impossiblepont(alt[i], disX[i], dPont, h, disX[0])
+
         if not impossible:
             break
 
--- main.py
+++ main.py
@@ -9,30 +9,7 @@ import calculs
 
 if __name__ == "__main__":
 
-    values, n, h, alpha, beta = read_file.read_file("testing/test5-1.in", data_separation=" ")
-    dis, alt, disAnt = calculs.obtainValues(values)
-
-    print("\n Values")
-    print("------------------ ")
+    values, n, h, alpha, beta = read_file.read_file("aqueductes/secret-08.in", data_separation=" ")
     print(values)
-    print("\n distancia entre cada punto")
-    print("------------------ ")
-    print(dis)
-    print(alt)
-    print("\n columnas/puntos")
-    print("------------------ ")
-    print(n)
-    print("\n Altura requerida")
-    print("------------------ ")
-    print(h)
-    print("\n Alpha")
-    print("------------------ ")
-    print(alpha)
-    print("\n Beta")
-    print("------------------ ")
-    print(beta)
-    print("\n Total sum")
-    print("------------------ ")
-
-    print(calculs.costsAque(n, alpha, beta, h, values))
+    """print(calculs.costsAque(n, alpha, beta, h, values))"""
     print(calculs.costPont(n, alpha, beta, h, values))
