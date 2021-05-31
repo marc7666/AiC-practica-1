a275c1bdbfef0ba35e5b289feb18e0583d8d6814
aaron-at97 <arenas.aaron97@gmail.com>
2021-05-31 16:26:50 +0200

    pylint acabado

--- calculs.py
+++ calculs.py
@@ -5,47 +5,43 @@
 # ************************************
 import math
 
-# distance_x todas las posiciones del vector
-# posant_x siempre el eje x_1
 
-'''
-This method calculates the possibility of creating a bridge
-'''
+def calc_impossible_pont(alt, distance_x, distance, height_aqueduct, pos_ant_x):
+    """
+    This method calculates the possibility of creating a bridge
+    """
 
-
-def calc_impossible_pont(alt, distance_x, distance, height_aqueduct, posant_x):
     radius = (distance / 2)
     height = math.sqrt((radius ** 2 - (
-            (distance_x - posant_x - radius) ** 2))
-                        ) + (height_aqueduct - radius)
+            (distance_x - pos_ant_x - radius) ** 2))
+                       ) + (height_aqueduct - radius)
     return height > alt
 
 
-'''
-This method calculates the possiblity of creating an aqueduct
-'''
-
-
 def calc_impossible(alt, distance, height_aqueduct):
+    """
+    This method calculates the possibility of creating an aqueduct
+    """
+
     radius = (distance / 2)
     height = (height_aqueduct - radius)
     return height > alt
 
 
-'''This method obtains de three different values: the distance between two columns (d), the different heights (
-coordinates Y), the different coordinates X 
-'''
-
-
 def obtain_values(values):
+    """This method obtains de three different values:
+    the distance between two columns (d), the different heights (coordinates Y),
+    the different coordinates X
+    """
+
     distance = []  # Distance
-    distance_x = []  # Distance Cordenate Sol
+    distance_x = []  # Distance Coordinate Sol
     alt = []  # Height
     ant_dis = -50
 
     for pos in values:
 
-        coordinate_x, coordinate_y = medides(pos)
+        coordinate_x, coordinate_y = measures(pos)
         alt.append(coordinate_y)
         distance_x.append(coordinate_x)
         if ant_dis != -50:
@@ -56,24 +52,22 @@ def obtain_values(values):
     return distance, alt, distance_x
 
 
-'''
-This method returns a tuple with a the two coordinates of a terrain point
-'''
+def measures(values):
+    """
+    This method returns a tuple with a the two coordinates of a terrain point
+    """
 
-
-def medides(values):
     coordinate_x = values[0]
     coordinate_y = values[1]
 
     return coordinate_x, coordinate_y
 
 
-'''
-This method calculates the costs of making an aqueduct
-'''
-
-
 def costs_aqueduct(terrain_points, alpha, beta, height_aqueduct, alt, distance):
+    """
+    This method calculates the costs of making an aqueduct
+    """
+
     costs_alt = 0
     costs_dis = 0
     impossible = True
@@ -92,12 +86,11 @@ def costs_aqueduct(terrain_points, alpha, beta, height_aqueduct, alt, distance):
     return cost, impossible
 
 
-'''
-This method calculates the costs of making a bridge
-'''
-
-
 def cost_pont(terrain_points, alpha, beta, height_aqueduct, alt, distance_x):
+    """
+    This method calculates the costs of making a bridge
+    """
+
     costs_alt_pont = (height_aqueduct - alt[0]) + (
             height_aqueduct - alt[terrain_points - 1]
     )
@@ -119,13 +112,12 @@ def cost_pont(terrain_points, alpha, beta, height_aqueduct, alt, distance_x):
     return cost, impossible
 
 
-'''
-This method will calculate the possibility of making a bridge and making an aqueduct and will return the best one 
-of the possiblities
-'''
-
-
 def calculate(terrain_points, alpha, beta, height_aqueduct, values):
+    """
+    This method will calculate the possibility of making a bridge and making an aqueduct and will return the best one
+    of the possibilities
+    """
+
     distance, alt, distance_x = obtain_values(values)
     if terrain_points == 2:
         cost2, impossible = costs_aqueduct(
