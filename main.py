5949b6cd37c4df69b68660ee568499f2016eca2b
aaron-at97 <arenas.aaron97@gmail.com>
2021-05-31 17:03:41 +0200

    pylint acabado

--- main.py
+++ main.py
@@ -3,43 +3,39 @@
 # Aaron Arenas Tomás
 # Marc Cervera Rosell
 # ************************************
-import argparse
-
 import read_file
 import calculs
 
-'''
-Main function of the iterative version of the practical case
-'''
-
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser(description='calculs')
-    # parser.add_argument('testing', help='Fichero del aqueducto')
-    # argumento = parser.parse_args()
+    """
+    Main function of the recursive version of the practical case
+    """
+
     i = 1
+
     for i in range(i, 21):
         if i < 10:
             num = "0" + str(i)
-            fitx = "aqueductes/secret-" + num + ".in"
+            file = "aqueductes/secret-" + num + ".in"
         else:
             num = str(i)
-            fitx = "aqueductes/secret-" + num + ".in"
-        values, n, h, alpha, beta = read_file.read_file(fitx, data_separation=" ")
+            file = "aqueductes/secret-" + num + ".in"
+        values, n, h, alpha, beta = read_file.read_file(file, data_separation=" ")
 
         cost = calculs.calculate(n, alpha, beta, h, values)
         print("---------------------------------------")
-        print("Fitx", fitx)
+        print("Fitx", file)
         print("---------------------------------------")
         print("Resultado introducido en el output.ans")
         print("---------------------------------------")
-        print("Resultado: ", cost)
+        print("Resultado RECURSIVO: ", cost)
         print("-------------------------")
         file_ans = open('output.ans', 'w')
         file_ans.write(str(cost) + '\n')
         file_ans.close()
-        fitx = "aqueductepont/secret-" + num + ".ap.ans"
+        file = "aqueductepont/secret-" + num + ".ap.ans"
 
-        with open(fitx) as a:
+        with open(file) as a:
             contentA = set(a)
 
         with open("output.ans") as b:
--- main_recursive.py
+++ main_recursive.py
@@ -3,34 +3,28 @@
 # Aaron Arenas Tomás
 # Marc Cervera Rosell
 # ************************************
-import argparse
 import sys
-
 import read_file
 import calculs_recursive
 
-'''
-Main function of the recursive version of the practical case
-'''
-
 if __name__ == "__main__":
-    #parser = argparse.ArgumentParser(description='calculs')
-    #parser.add_argument('testing', help='Fichero del aqueducto')
-    #argumento = parser.parse_args()
+    """
+    Main function of the recursive version of the practical case
+    """
     sys.setrecursionlimit(50000)
     i = 1
     for i in range(i, 21):
         if i < 10:
             num = "0" + str(i)
-            fitx = "aqueductes/secret-" + num + ".in"
+            file = "aqueductes/secret-" + num + ".in"
         else:
             num = str(i)
-            fitx = "aqueductes/secret-" + num + ".in"
-        values, n, h, alpha, beta = read_file.read_file(fitx, data_separation=" ")
+            file = "aqueductes/secret-" + num + ".in"
+        values, n, h, alpha, beta = read_file.read_file(file, data_separation=" ")
 
         cost = calculs_recursive.calculate(n, alpha, beta, h, values)
         print("---------------------------------------")
-        print("Fitx", fitx)
+        print("Fitx", file)
         print("---------------------------------------")
         print("Resultado introducido en el output.ans")
         print("---------------------------------------")
@@ -39,9 +33,9 @@ if __name__ == "__main__":
         file_ans = open('output.ans', 'w')
         file_ans.write(str(cost) + '\n')
         file_ans.close()
-        fitx = "aqueductepont/secret-" + num + ".ap.ans"
+        file = "aqueductepont/secret-" + num + ".ap.ans"
 
-        with open(fitx) as a:
+        with open(file) as a:
             contentA = set(a)
 
         with open("output.ans") as b:
