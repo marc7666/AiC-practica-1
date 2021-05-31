2ee66cad477fd3004e917a3d4587ae131628731a
aaron-at97 <arenas.aaron97@gmail.com>
2021-05-31 17:31:35 +0200

    pylint acabado

--- read_file.py
+++ read_file.py
@@ -13,14 +13,14 @@ def read_file(filename, data_separation=" "):
     print("************* Fichero cargado *************")
     values = []  # List of tuples (x, y)
     # Openning file
-    with open(filename, "r") as fn:
+    with open(filename, "r") as vec_file_name:
         # Strip lines
-        strip_reader = (line.strip() for line in fn)
+        strip_reader = (line.strip() for line in vec_file_name)
         filtered_reader = [line for line in strip_reader if line]
         # First line is the problem data
-        n, h, alpha, beta = map(int, filtered_reader[0].split(data_separation))
+        terrain_points, height_aqueduct, alpha, beta = map(int, filtered_reader[0].split(data_separation))
         # Split lines, parse token and append to values (2 to last)
         for line in filtered_reader[1:]:
-            x, y = map(int, line.split(data_separation))
-            values.append((x, y))
-        return values, n, h, alpha, beta
+            coordinate_x, coordinate_y = map(int, line.split(data_separation))
+            values.append((coordinate_x, coordinate_y))
+        return values, terrain_points, height_aqueduct, alpha, beta
